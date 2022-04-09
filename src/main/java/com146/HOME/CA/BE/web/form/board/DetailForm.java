2e9b11c1f190622385fb2a83ef418fe967fd4e49
BoKyeong Kwon <dudrud7473@gmail.com>
2022-04-09 19:10:54 +0900

    권보경6

--- src/main/java/com146/HOME/CA/BE/web/form/board/DetailForm.java
+++ src/main/java/com146/HOME/CA/BE/web/form/board/DetailForm.java
@@ -2,6 +2,8 @@ package com146.HOME.CA.BE.web.form.board;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class DetailForm {
 
@@ -15,5 +17,13 @@ public class DetailForm {
     private String boardContent;	    //CLOB --게시글 내용
     private String fromRecipe;          //varchar2(30) --레시피 출처
     private String boardMapAddress;	//VARCHAR2(300) --지도 API에서 받은 주소
+
+    private Long ReplyNum;                    //댓글번호 NUMBER(10)
+    private LocalDate replyDate;              //작성일 TIMESTAMP
+    private String replyContent;              //댓글내용 CLOB
+    private int replyGroup;                   //댓글그룹 NUMBER(5)
+    private int replyStep;                    //댓글의 단계 NUMBER(5)
+    private int replyIndent;                  //댓글의 들여쓰기 NUMBER(5)
+    private int preplyNum;                    //부모댓글번호 NUMBER(10)
 
 }
